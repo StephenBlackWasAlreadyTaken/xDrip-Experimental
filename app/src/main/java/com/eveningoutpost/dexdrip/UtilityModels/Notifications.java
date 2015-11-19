8b1c0b6b5d73011d3a0773fdecc512db2d8f28b0
Matthias Granberry <matthias.granberry@tabbedout.com>
2015-11-18 21:13:51 -0600

    Log wake time message in human-readable time

--- app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/Notifications.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/Notifications.java
@@ -300,7 +300,7 @@ public class Notifications extends IntentService {
                 // sleep longer if the alert is snoozed.
                 long wakeTime = activeBgAlert.is_snoozed ? activeBgAlert.next_alert_at :
                         activeBgAlert.last_alerted_at + (time * 60000);
-                Log.d(TAG , "ArmTimer waking at: "+ wakeTime +" in minutes: " +  (wakeTime - calendar.getTimeInMillis())/60000d);
+                Log.d(TAG , "ArmTimer waking at: "+ new Date(wakeTime) +" in minutes: " +  (wakeTime - calendar.getTimeInMillis())/60000d);
                 if (wakeIntent != null)
                     alarm.cancel(wakeIntent);
                 wakeIntent = PendingIntent.getService(this, 0, new Intent(this, this.getClass()), 0);
