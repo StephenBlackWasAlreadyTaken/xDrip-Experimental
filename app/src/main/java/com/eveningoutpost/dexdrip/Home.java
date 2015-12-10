caafad108faf0ea93257fbfa68f21ac15d5d3021
jstevensog <jstevensog@gmail.com>
2015-12-10 12:26:50 +1100

    Modified per Johan Degraeve's comments.
    Minor changes to the logic around showing the Bridge Battery.

--- app/src/main/java/com/eveningoutpost/dexdrip/Home.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Home.java
@@ -380,9 +380,10 @@ public class Home extends ActivityWithMenu {
 
         if (isDexbridge && displayBattery) {
             if (bridgeBattery == 0) {
-                dexbridgeBattery.setText("Waiting for packet");
+                dexbridgeBattery.setText("sBridge Battery unknown: Waiting for packet");
+                dexbridgeBattery.setTextColor(Color.WHITE);
             } else {
-                dexbridgeBattery.setText("Bridge Battery: " + bridgeBattery + "%");
+                dexbridgeBattery.setText("xBridge Battery: " + bridgeBattery + "%");
             }
             if (bridgeBattery < 50) dexbridgeBattery.setTextColor(Color.YELLOW);
             if (bridgeBattery < 25) dexbridgeBattery.setTextColor(Color.RED);
