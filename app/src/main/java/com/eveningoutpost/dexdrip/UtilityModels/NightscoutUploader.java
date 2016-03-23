dcba03a365d5f0f661db9b1c2c7ebcf283122061
Tzachi Dar <tzachi.dar@gmail.com>
2016-03-24 00:24:03 +0200

    Add timeout on mongo send/receive operations.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/NightscoutUploader.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/NightscoutUploader.java
@@ -324,7 +324,7 @@ public class NightscoutUploader {
                 try {
 
                     // connect to db
-                    MongoClientURI uri = new MongoClientURI(dbURI.trim());
+                    MongoClientURI uri = new MongoClientURI(dbURI.trim()+"?socketTimeoutMS=180000");
                     MongoClient client = new MongoClient(uri);
 
                     // get db
