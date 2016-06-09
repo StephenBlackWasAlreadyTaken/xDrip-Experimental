acf8997f39a08d3347d0cd602028b67a82c4990f
tzachi <tzachi@localhost>
2016-06-10 00:44:53 +0300

    Basic functionality of disable alerts works.

--- app/src/main/java/com/eveningoutpost/dexdrip/EditAlertActivity.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/EditAlertActivity.java
@@ -63,6 +63,7 @@ public class EditAlertActivity extends ActivityWithMenu {
     private Button buttonPreSnooze;
     private CheckBox checkboxAllDay;
     private CheckBox checkboxVibrate;
+    private CheckBox checkboxDisabled;
 
     private LinearLayout layoutTimeBetween;
     private LinearLayout timeInstructions;
@@ -77,9 +78,9 @@ public class EditAlertActivity extends ActivityWithMenu {
     private int endMinute = 59;
 
 
-    private int defaultSnooze;
+    private int defaultSnooze;   /// remove member variable  ?????????????
 
-    private String audioPath;
+    private String audioPath;    ///  remove member variable ????????????
 
     private TextView viewAlertOverrideText;
     private CheckBox checkboxAlertOverride;
@@ -130,6 +131,7 @@ public class EditAlertActivity extends ActivityWithMenu {
 
         checkboxAllDay = (CheckBox) findViewById(R.id.check_alert_time);
         checkboxVibrate = (CheckBox) findViewById(R.id.check_vibrate);
+        checkboxDisabled = (CheckBox) findViewById(R.id.view_alert_check_disable);
 
         layoutTimeBetween = (LinearLayout) findViewById(R.id.time_between);
         timeInstructions = (LinearLayout) findViewById(R.id.time_instructions);
@@ -161,6 +163,7 @@ public class EditAlertActivity extends ActivityWithMenu {
 
             checkboxAllDay.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
             checkboxVibrate.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+            checkboxDisabled.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
 
             viewTimeStart.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
             viewTimeEnd.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
@@ -193,6 +196,7 @@ public class EditAlertActivity extends ActivityWithMenu {
             above = Boolean.parseBoolean(getExtra(savedInstanceState, "above"));
             checkboxAllDay.setChecked(true);
             checkboxVibrate.setChecked(true);
+            checkboxDisabled.setChecked(false);
             checkboxAlertOverride.setChecked(true);
 
             audioPath = "";
@@ -224,6 +228,7 @@ public class EditAlertActivity extends ActivityWithMenu {
             alertThreshold.setText(unitsConvert2Disp(doMgdl, at.threshold));
             checkboxAllDay.setChecked(at.all_day);
             checkboxVibrate.setChecked(at.vibrate);
+            checkboxDisabled.setChecked(!at.active);
             checkboxAlertOverride.setChecked(at.override_silent_mode);
             defaultSnooze = at.default_snooze;
             if(defaultSnooze == 0) {
@@ -467,13 +472,14 @@ public class EditAlertActivity extends ActivityWithMenu {
                     return;
                 }
                 boolean vibrate = checkboxVibrate.isChecked();
+                boolean disabled = checkboxDisabled.isChecked();
                 boolean overrideSilentMode = checkboxAlertOverride.isChecked();
 
                 String mp3_file = audioPath;
                 if (uuid != null) {
-                    AlertType.update_alert(uuid, alertText.getText().toString(), above, threshold, allDay, alertReraise, mp3_file, timeStart, timeEnd, overrideSilentMode, defaultSnooze, vibrate);
+                    AlertType.update_alert(uuid, alertText.getText().toString(), above, threshold, allDay, alertReraise, mp3_file, timeStart, timeEnd, overrideSilentMode, defaultSnooze, vibrate, !disabled);
                 }  else {
-                    AlertType.add_alert(null, alertText.getText().toString(), above, threshold, allDay, alertReraise, mp3_file, timeStart, timeEnd, overrideSilentMode, defaultSnooze, vibrate);
+                    AlertType.add_alert(null, alertText.getText().toString(), above, threshold, allDay, alertReraise, mp3_file, timeStart, timeEnd, overrideSilentMode, defaultSnooze, vibrate, !disabled);
                 }
                 Intent returnIntent = new Intent();
                 setResult(RESULT_OK,returnIntent);
