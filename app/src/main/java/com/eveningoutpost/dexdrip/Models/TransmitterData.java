6d46e67c5ae004f92c973c746c02b6d7af0f2840
Timm Korte <korte-github@easycrypt.de>
2016-07-17 22:36:04 +0200

    add support for xBridge 2 with queue

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/TransmitterData.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/TransmitterData.java
@@ -10,7 +10,10 @@ import com.activeandroid.query.Select;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 /**
@@ -36,10 +39,20 @@ public class TransmitterData extends Model {
     @Column(name = "uuid", index = true)
     public String uuid;
 
+    public static List<TransmitterData> readingNearTimeStamp(long timestamp, double margin) {
+            return new Select()
+                    .from(TransmitterData.class)
+                    .where("raw_data != 0") // if the last one didn't have a value, we'd rather take the new one
+                    .where("timestamp >= " + (timestamp-margin)) // if we already have a reading up to margin ms before that timestamp
+                    .where("timestamp <= " + (timestamp+margin)) // or there is within margin ms after the given timestamp
+                    .orderBy("timestamp desc")
+                    .execute();
+    }
+
     public static synchronized TransmitterData create(byte[] buffer, int len, Long timestamp) {
         if (len < 6) { return null; }
         TransmitterData transmitterData = new TransmitterData();
-        if (buffer[0] == 0x11 && buffer[1] == 0x00) {
+        if ((buffer[0] == 0x11 || buffer[0] == 0x15) && buffer[1] == 0x00) {
             //this is a dexbridge packet.  Process accordingly.
             Log.i(TAG, "create Processing a Dexbridge packet");
             ByteBuffer txData = ByteBuffer.allocate(len);
@@ -49,7 +62,13 @@ public class TransmitterData extends Model {
             transmitterData.filtered_data = txData.getInt(6);
             //  bitwise and with 0xff (1111....1) to avoid that the byte is treated as signed.
             transmitterData.sensor_battery_level = txData.get(10) & 0xff;
-            Log.i(TAG, "Created transmitterData record with Raw value of " + transmitterData.raw_data + " and Filtered value of " + transmitterData.filtered_data + " at " + transmitterData.timestamp);
+            if (buffer[0] == 0x15) {
+                Log.i(TAG, "create Processing a Dexbridge packet includes delay information");
+                transmitterData.timestamp = timestamp - txData.getInt(16);
+            } else {
+                transmitterData.timestamp = timestamp;
+            }
+            Log.i(TAG, "Created transmitterData record with Raw value of " + transmitterData.raw_data + " and Filtered value of " + transmitterData.filtered_data + " at " + timestamp + " with timestamp " + transmitterData.timestamp);
         } else { //this is NOT a dexbridge packet.  Process accordingly.
             Log.i(TAG, "create Processing a BTWixel or IPWixel packet");
             StringBuilder data_string = new StringBuilder();
@@ -61,6 +80,7 @@ public class TransmitterData extends Model {
             }
             transmitterData.raw_data = Integer.parseInt(data[0]);
             transmitterData.filtered_data = Integer.parseInt(data[0]);
+            transmitterData.timestamp = timestamp;
         }
         //Stop allowing duplicate data, its bad!
         TransmitterData lastTransmitterData = TransmitterData.last();
@@ -68,7 +88,17 @@ public class TransmitterData extends Model {
             return null;
         }
 
-        transmitterData.timestamp = timestamp;
+        //Default dupes due to missing ack are filtered by the function above - so this protects against duplicates when using queues on multiple bridges
+        final List<TransmitterData> possibleDuplicates = readingNearTimeStamp(timestamp, (3*60*1000));
+        if (possibleDuplicates != null && possibleDuplicates.size() > 0) {
+            Log.i(TAG, "TransmitterData.create: Received Packet where we already have another reading for the same timeslot. Exiting.");
+            final DateFormat dateFormatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.getDefault());
+            for (TransmitterData reading : possibleDuplicates) {
+                Log.i(TAG, "  Possible duplicate with offset " + (timestamp - reading.timestamp) + " ms to reading at " + dateFormatter.format(new Date(reading.timestamp)) + ", raw: " + transmitterData.raw_data + " vs. " + reading.raw_data + ".");
+            }
+            return null;
+        }
+
         transmitterData.uuid = UUID.randomUUID().toString();
         transmitterData.save();
         return transmitterData;
